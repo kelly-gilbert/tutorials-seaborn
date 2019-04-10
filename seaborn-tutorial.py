338e9d2cacc1d24660942aa450c255ff4437b33d
Kelly Gilbert <gilbert.kelly.lynn@gmail.com>
2019-04-09 22:53:23 -0400

    created new repository

---
+++ seaborn-tutorial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 22:29:05 2019
+
+@author: Kelly
+"""
+
+
+import numpy as np
+import pandas as pd
+import maplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style='darkgrid')
